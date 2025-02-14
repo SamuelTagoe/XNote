5cf552f9872af6cfe1724dcc390374c6adc1fcc7
Jhay <jhaycodes999@example.com>
2025-02-13 19:06:14 -0800

    Initial Commit

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':  # Only if this file runs, app.run()
     app.run(debug = True)     # debug=True which means the webserver should auto run anytime we make changes
+    
